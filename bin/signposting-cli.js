6750fec97b0241af028971a49651a411ab7b369e
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2025-07-29 14:10:31 +0200

    Supporting linksets

--- bin/signposting-cli.js
+++ bin/signposting-cli.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const { signpostingInfo } = require('../lib/signposting.js');
+const { signpostingInfo , linkSetInfo } = require('../lib/signposting.js');
 
 program
   .name('signposting-cli')
   .description('CLI to FAIR Signposting')
+
+program.command('signposting')
   .argument('<url>', 'url to fetch')
   .action( async (url) => {
      const info = await signpostingInfo(url,{});
+     console.log(JSON.stringify(info,null,2));
+  });
+
+program.command('linkset') 
+  .argument('<url>', 'url to fetch')
+  .action( async (url) => {
+     const info = await linkSetInfo(url,{});
      console.log(JSON.stringify(info,null,2));
   });
 
